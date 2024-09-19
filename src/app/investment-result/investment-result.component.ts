8ce9f2e4245d012a6aaa7fde5cd094562cd68344
khaled <dev.khlaed.mohamed@gmail.com>
2024-09-19 15:27:19 +0300

    add the investmentResults component

---
+++ src/app/investment-result/investment-result.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'investment-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-result.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultComponent {
+
+}
